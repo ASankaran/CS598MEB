0993d8f4f59ded024cffbee47fe31b820e41b76b
Arnav Sankaran <arnavsankaran@gmail.com>
2020-04-18 20:35:13 -0500

    Use chromosomes as channels

--- cnn.py
+++ cnn.py
@@ -7,17 +7,16 @@ from common import classifications
 class ConvMutationNet(nn.Module):
 	def __init__(self):
 		super().__init__()
-		self.cv01 = nn.Conv1d(1, 2, kernel_size=5)
-		self.cv02 = nn.Conv1d(2, 2, kernel_size=5)
-		self.cv03 = nn.Conv1d(2, 4, kernel_size=5)
-		self.cv04 = nn.Conv1d(4, 4, kernel_size=5)
-		self.cv05 = nn.Conv1d(4, 8, kernel_size=5)
-		self.cv06 = nn.Conv1d(8, 8, kernel_size=5)
+		self.cv01 = nn.Conv1d(23, 32, kernel_size=5)
+		self.cv02 = nn.Conv1d(32, 32, kernel_size=5)
+		self.cv03 = nn.Conv1d(32, 64, kernel_size=5)
+		self.cv04 = nn.Conv1d(64, 64, kernel_size=5)
+		self.cv05 = nn.Conv1d(64, 128, kernel_size=5)
+		self.cv06 = nn.Conv1d(128, 128, kernel_size=5)
 		self.pool = nn.MaxPool1d(2, 2)
-		self.fc01 = nn.Linear(8 * 711, len(classifications))
+		self.fc01 = nn.Linear(128 * 24, len(classifications))
 
 	def forward(self, x):
-		x = (x > 1).float()
 		x = F.relu(self.cv01(x))
 		x = F.relu(self.cv02(x))
 		x = self.pool(x)
@@ -27,6 +26,6 @@ class ConvMutationNet(nn.Module):
 		x = F.relu(self.cv05(x))
 		x = F.relu(self.cv06(x))
 		x = self.pool(x)
-		x = x.view(-1, 8 * 711)
+		x = x.view(-1, 128 * 24)
 		x = self.fc01(x)
 		return x
